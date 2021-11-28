12905f535933b96e42420bd2a5e03bc138be0407
Leo <danielesquivel00.lded@gmail.com>
2021-11-27 22:14:54 -0600

    [ADD] Campo cart agregado a usuario

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -7,7 +7,11 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
-    admin: {type: Boolean, required: true}
+    admin: {type: Boolean, required: true},
+    cart: {
+        type: Schema.Types.ObjectId,
+        ref: 'cart',
+    }
 });
 
 UserSchema.methods.toJSON = function(){
